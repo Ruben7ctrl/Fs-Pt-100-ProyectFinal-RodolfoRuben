075c7f9f3a66596d090d16ecd1249f480bb4dffc
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-05-25 18:13:37 +0000

    Page Home iniciada sin navbar/footer

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -6,10 +6,10 @@ import { Footer } from "../components/Footer"
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
-        <ScrollToTop>
-            <Navbar />
+        // <ScrollToTop>
+        //     <Navbar />
                 <Outlet />
-            <Footer />
-        </ScrollToTop>
+        //     <Footer />
+        // </ScrollToTop>
     )
 }
