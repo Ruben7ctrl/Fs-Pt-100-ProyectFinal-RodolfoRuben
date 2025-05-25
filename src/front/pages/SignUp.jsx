11f6a2c5ac87bb929ce44b97c18a6bb00e187344
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-05-25 15:38:08 +0000

    Autentication funcional con estilos iniciales (Pages y flux)

--- src/front/pages/SignUp.jsx
+++ src/front/pages/SignUp.jsx
@@ -87,47 +87,47 @@ export const Signup = () => {
                         <h2>SignUp</h2>
                     </div>
                     <div className=" mt-3 col-sm-12 col-md-12">
-                        <label htmlFor="email" className="form-label">Email</label>
+                        {/* <label htmlFor="email" className="form-label">Email</label> */}
                         <input type="email" className="form-control" placeholder="Email" name="email" value={formData.email} onChange={handleChange} required />
                     </div>
                     <div className=" mt-3 col-sm-12 col-md-12">
-                        <label htmlFor="password" className="form-label">Password</label>
+                        {/* <label htmlFor="password" className="form-label">Password</label> */}
                         <input type="password" className="form-control" placeholder="Password" name="password" value={formData.password} onChange={handleChange} required />
                     </div>
                     <div className=" mt-3 col-sm-12 col-md-12">
-                        <label htmlFor="inputUserName" className="form-label">UserName</label>
+                        {/* <label htmlFor="inputUserName" className="form-label">UserName</label> */}
                         <input type="text" className="form-control" id="inputUserName" placeholder="UserName" name="username" value={formData.username} onChange={handleChange} required />
                     </div>
                     <div className="row mt-3">
                         <div className="col-sm-12 col-md-6">
-                            <label htmlFor="inputName" className="form-label">First Name</label>
+                            {/* <label htmlFor="inputName" className="form-label">First Name</label> */}
                             <input type="text" className="form-control" placeholder="First Name" id="inputName" name="firstname" value={formData.firstname} onChange={handleChange} required />
                         </div>
                         <div className="col-sm-12 col-md-6">
-                            <label htmlFor="inputLastName" className="form-label">Last Name</label>
+                            {/* <label htmlFor="inputLastName" className="form-label">Last Name</label> */}
                             <input type="text" className="form-control" placeholder="Last Name" id="inputLastName" name="lastname" value={formData.lastname} onChange={handleChange} required />
                         </div>
                         <div className="row mt-3">
                             <div className="col-sm-12 col-md-6">
-                                <label className="form-label" htmlFor="date-of-birth">
+                                {/* <label className="form-label" htmlFor="date-of-birth">
                                     Date of Birth
-                                </label>
+                                </label> */}
                                 <input className="form-control" type="text" inputMode="numeric" id="date-of-birth" name="dateofbirth" placeholder="dd/mm/yyyy" value={formData.dateofbirth} onChange={handleChange} required />
                             </div>
                             <div className="col-sm-12 col-md-6">
-                                <label htmlFor="inputPhone" className="form-label">Phone</label>
+                                {/* <label htmlFor="inputPhone" className="form-label">Phone</label> */}
                                 <input type="number" className="form-control" placeholder="Phone" id="inputPhone" name="phone" value={formData.phone} onChange={handleChange} required />
                             </div>
                         </div>
                     </div>
                     <div className=" mt-3 col-sm-12 col-md-12">
-                        <label htmlFor="avatar" className="form-label">Avatar</label>
+                        {/* <label htmlFor="avatar" className="form-label">Avatar</label> */}
                         <input type="text" className="form-control" placeholder="Avatar" name="avatar" value={formData.avatar_image} onChange={handleChange} required />
                     </div>
 
-                    <div className="d-flex justify-content-between gap-3 mt-3">
+                    <div className="d-flex flex-column justify-content-center gap-3 mt-3">
                         <button className="btn btn-secondary" onClick={handleClick}>Already account?</button>
-                        <input type="submit" className="btn btn-primary" />
+                        <input type="submit" className="btn btn-primary" value={"Sign Up"}/>
                     </div>
 
 
