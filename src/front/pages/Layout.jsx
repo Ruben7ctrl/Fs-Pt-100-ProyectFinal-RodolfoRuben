b4f6734a2824020a34232e341b5da412b7fb35cf
Ruben7ctrl <rubencabellomiguel@gmail.com>
2025-05-26 08:48:01 +0000

    Cambios en layout

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -6,10 +6,10 @@ import { Footer } from "../components/Footer"
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
-        // <ScrollToTop>
-        //     <Navbar />
+        <ScrollToTop>
+            {localStorage.getItem("token") && <Navbar />}
                 <Outlet />
-        //     <Footer />
-        // </ScrollToTop>
+            {localStorage.getItem("token") && <Footer />}
+        </ScrollToTop>
     )
 }
