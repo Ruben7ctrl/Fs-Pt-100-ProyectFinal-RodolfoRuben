1c12a64ee458c8fdf672551526569371f3ba5cfd
hedrickantonio <rhantoni.cuba@gmail.com>
2025-05-29 13:53:10 +0000

    home2

--- src/front/pages/Games.jsx
+++ src/front/pages/Games.jsx
@@ -121,21 +121,21 @@ export const Games = () => {
     <>
       <div className="fondoGames">
         <div className="container">
-      <div className="ps5-grid">
-        {items.map(({ icon, label }, i) => (
-          <div
-            key={i}
-            className="char ps5-card"
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-          >
-            <div className="icon">{icon}</div>
-            <span className="label">{label}</span>
-          
+          <div className="ps5-grid">
+            {items.map(({ icon, label }, i) => (
+              <div
+                key={i}
+                className="char ps5-card"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                <div className="icon">{icon}</div>
+                <span className="label">{label}</span>
+
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
         <div className="discover-sidebar__nav__elements">
           <div className="page__content-wrap-centerer">
             <div className="page__content-wrap with-sidebar">
